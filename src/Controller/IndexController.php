1208ce37a3cb76406ad5890253e650a0548a5d7d
James Jones <james.jones@boxuk.com>
2023-02-10 14:04:46 +0000

    navigation and home directory added

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class IndexController extends AbstractController
+{
+    public function indexAction()
+    {
+        return $this->render('main.html.twig', [
+            'title' => 'Home of Symfony Learning'
+        ]);
+    }
+}
